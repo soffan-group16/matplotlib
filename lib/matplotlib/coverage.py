5280b797dd4bbd92860cf1f5d9f68a48cb556dbe
Markus Wesslén <markus.wesslen@gmail.com>
2021-02-23 16:59:23 +0100

    Update coverage with static reports path

--- lib/matplotlib/coverage.py
+++ lib/matplotlib/coverage.py
@@ -3,16 +3,16 @@ import sys
 
 
 class Coverage:
-
     def __init__(self, branch_count, function_name):
         try:
             self.test_mode = int(os.environ["COV_TEST"])
         except (KeyError, ValueError):
-            self.test_mode=0
+            self.test_mode = 0
         if self.test_mode:
             self.branch_count = branch_count
             self.function_name = function_name
             self.log_list = [False for _ in range(branch_count)]
+            self.reports_folder = os.environ["COV_REPORTS_PATH"]
 
     def log(self, id):
         if not self.test_mode:
@@ -25,7 +25,9 @@ class Coverage:
     def report(self):
         if not self.test_mode:
             return
-        with open(f"{self.function_name}.report", "a") as output_file:
+        with open(
+            f"{self.reports_folder}{self.function_name}.report", "a"
+        ) as output_file:
             output_file.write(f"{self.branch_count},")
             for id, log in enumerate(self.log_list):
                 if log:
