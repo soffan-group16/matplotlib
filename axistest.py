e3420d687045f5d8f81f74494d14bc95d59eeb98
Markus Wesslén <markus.wesslen@gmail.com>
2021-03-05 09:01:14 +0100

    Update test script

--- axistest.py
+++ axistest.py
@@ -4,13 +4,23 @@ import matplotlib.pyplot as plt
 def render_scaling():
     fig = plt.figure()
     ax = fig.gca(projection = '3d')
-    plt.show()
+    ax.plot([0,1,2], [0,1,2], [0,1,2])
 
 
 def render_non_scaling():
     fig = plt.figure()
     ax = fig.gca(projection = '3d')
-    ax.set_xlim(0,0.8)
-    plt.show()
+    ax.set_zmargin(0.05)
+    ax.plot([0,1], [0,1], [0,1])
+    #ax.set_xlim(0,1)
+    #ax.set_ylim(0,1)
+    print(ax.get_xbound())
+    print(ax.get_ybound())
+    print(ax.get_zbound())
+    print(ax._xmargin)
+    print(ax._ymargin)
+    print(ax._zmargin)
 
+render_scaling()
 render_non_scaling()
+plt.show()
